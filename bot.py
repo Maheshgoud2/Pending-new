2fcdad29d5d88fac0cd1bd66fadf96a5ff196583
Muhammed Ashik <116175002+mhdashikofficial@users.noreply.github.com>
2025-01-31 10:32:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from pyrogram.errors import FloodWait, PeerIdInvalid, RPCError
 
 logging.basicConfig(level=logging.ERROR)
 
-API_ID = 24323145  # Replace with your API ID
-API_HASH = "2255461d65e3ad2440a936bc078b301f"  # Replace with your API hash
+API_ID =   # Replace with your API ID
+API_HASH = ""  # Replace with your API hash
 
 app = Client("user_account_session", api_id=API_ID, api_hash=API_HASH)
 
